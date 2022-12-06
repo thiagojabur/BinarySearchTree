6878909fd82a9b9e562f1895ad299a759696d0b8
Thiago Jabur Bittar <31863285+thiagojabur@users.noreply.github.com>
2022-12-06 17:06:33 -0300

    Update Principal.java

--- src/Principal.java
+++ src/Principal.java
@@ -6,30 +6,35 @@ public class Principal {
 		BinarySearchTree arvoreBinariaTeste = new BinarySearchTree(4);
 		arvoreBinariaTeste.insert(3);
 		arvoreBinariaTeste.insert(1);
-		arvoreBinariaTeste.insert(2);
+		arvoreBinariaTeste.insert(14);
 		arvoreBinariaTeste.insert(3);
 		arvoreBinariaTeste.insert(10);
-		arvoreBinariaTeste.insert(5);
+		arvoreBinariaTeste.insert(18);
+		arvoreBinariaTeste.insert(11);
 		
 		System.out.println("Grau da Raiz: " +  arvoreBinariaTeste.getRoot().length());
-		System.out.print("PrÈ ordem: ");
+		System.out.print("Pr√© ordem: ");
 		arvoreBinariaTeste.preOrderPrint();
 		System.out.println("");
 		System.out.print("Em ordem: ");
 		arvoreBinariaTeste.inOrderPrint();
 		System.out.println("");
-		System.out.print("PÛs ordem: ");
+		System.out.print("P√≥s ordem: ");
 		arvoreBinariaTeste.postOrderPrint();
 		System.out.println("");
-		System.out.println("… estritamente bin·ria: ");
+		System.out.println("√â estritamente bin√°ria: ");
 		System.out.print(arvoreBinariaTeste.isStrictBinaryTree());
 		System.out.println("");
-		System.out.println("Quantidade de nÛs: ");
+		System.out.println("Quantidade de n√≥s: ");
 		System.out.println(arvoreBinariaTeste.findNodeAmount());
 
-		System.out.println("Quantidade m·xima de nÌveis (profundidade): ");
+		System.out.println("Quantidade m√°xima de n√≠veis (profundidade): ");
 		System.out.println(arvoreBinariaTeste.findDepth());
-	
+		int buscado = 8;
+		System.out.println("Buscou o " + buscado + ": " + 
+		  arvoreBinariaTeste.search(buscado));
+		
+		
 	}
 
 }
